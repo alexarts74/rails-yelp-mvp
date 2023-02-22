1da10a2b9dfe0a27fe2469809529ab76cfab47a1
alexandre artus <artusalexandre@yahoo.fr>
2023-02-22 17:09:09 +0100

    Rails restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
+require 'faker'
+
+puts "go"
+
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(
+    name: Faker::Name.name,
+    category: Restaurant::CATEGORIES.sample,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+puts "finiiiii"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
